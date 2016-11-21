cd609b7331588b94084dea9d770ac5ba7332eabc
Arthi Arumugam <arthi.arumugam@perspectum-diagnostics.com>
2016-11-21 15:22:22 +0000

    adding crm settings

--- crm/settings.py
+++ crm/settings.py
@@ -56,14 +56,6 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'crm.urls'
 
-
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
-)
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
