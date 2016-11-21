bb9afd58929b2cfe464a567587af0e11e87537c8
Arthi Arumugam <arthi.arumugam@perspectum-diagnostics.com>
2016-11-21 14:53:00 +0000

    changing settings for static files

--- crm/settings.py
+++ crm/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = '/admin/css'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
