299616fdc33f5a15e88131db6c9b1a387eb0d3c5
Arthi Arumugam <arthi.arumugam@perspectum-diagnostics.com>
2016-11-21 15:16:28 +0000

    modifying crm/settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
